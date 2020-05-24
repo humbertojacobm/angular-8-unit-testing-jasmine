d8b1f89ba1302220e13754dbbea030d1a75878ef
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-24 11:59:28 -0500

    18835, how to mock services in a component, using of, and how to mock material dependency

--- src/app/components/form/form.component.spec.ts
+++ src/app/components/form/form.component.spec.ts
@@ -1,14 +1,52 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async,
+         ComponentFixture,
+         TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { of } from 'rxjs';
+import { NavigationService } from 'src/app/services/navigation.service';
+import { MatSnackBar } from '@angular/material';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  class RepositoryServiceStub{
+    savePins(){
+      return of(true);
+    }
+  }
+  class NavigationServiceStub{
+    goToPins(){
 
+    }
+  }
+  class MatSnackBarStub{
+    open(){
+      return {
+        afterDismissed: () => {
+          return of(true);
+        }
+      }
+    }
+  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
+      declarations: [ FormComponent ],
+      providers: [
+        {
+          provide: RepositoryService,
+          useClass: RepositoryServiceStub
+        },
+        {
+          provide: NavigationService,
+          useClass: NavigationServiceStub
+        },
+        {
+          provide: MatSnackBar,
+          useClass: MatSnackBarStub,
+        }
+      ]
     })
     .compileComponents();
   }));
