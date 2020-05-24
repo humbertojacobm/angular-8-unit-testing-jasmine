5560bfcd910e89bcdb83eaa8b02075af8410be3e
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-24 11:03:26 -0500

    configuring a test bed for a component that is using router-outlet app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,9 @@
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { TestBed,
+         async} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterOutlet } from '@angular/router';
 
-fdescribe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let component: AppComponent;
+describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,14 +12,4 @@ fdescribe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.debugElement.componentInstance;
-  });
-
-  it('Should have a router outlet', () => {
-    const de = fixture.debugElement.query(By.directive(RouterOutlet));
-
-    expect(de).not.toBeNull();
-  });
 });
