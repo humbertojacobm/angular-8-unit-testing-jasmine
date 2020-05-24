c0fe3371d503587695ef64e6046698c7f1c475c0
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-05-24 11:08:29 -0500

    using ComponentFixture

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,26 @@
 import { TestBed,
-         async} from '@angular/core/testing';
+         async,
+         ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent]
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  })
+
+  it('should have a router outlet', ()=>{
+
+  });
 
 });
