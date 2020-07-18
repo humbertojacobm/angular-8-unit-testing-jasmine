e064ef55c9e464f5e047c7506b2b69456c3e229b
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-07-18 00:08:41 -0500

    first unit test using query and by to check if we have a directive

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,33 @@ import { TestBed,
          async,
          ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+fdescribe('AppComponent', ()=> {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  beforeEach( async(()=>{
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
+    }).compileComponents();
+  }))
+
+  beforeEach(()=> {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  });
+
+  it('should have a router outlet', ()=> {
+    const de = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(de).not.toBeNull();
+  });
+
+});
 
 // fdescribe('AppComponent', () => {
 //   let fixture: ComponentFixture<AppComponent>;
