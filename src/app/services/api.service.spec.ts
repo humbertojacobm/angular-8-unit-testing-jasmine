a6297b89396f1c8c94993ba4765c7403832a0d1c
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-07-22 16:00:48 -0500

    finishing unit test in services using post, delete, put

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -2,6 +2,8 @@ import { ApiService } from "./api.service"
 import { TestBed, getTestBed } from "@angular/core/testing";
 import { HttpTestingController, HttpClientTestingModule } from "@angular/common/http/testing"
 import { environment } from "src/environments/environment";
+import { inject } from "@angular/core";
+import { HttpHeaders } from "@angular/common/http";
 
 fdescribe("api servicve",()=>{
   it("should be constructed",()=>{
@@ -46,8 +48,6 @@ fdescribe("api servicve",()=>{
     service = TestBed.get(ApiService);
     httpMock = injector.get(HttpTestingController);
 
-
-    debugger;
     const parameter = "/test";
     const response = "testing";
 
@@ -69,5 +69,160 @@ fdescribe("api servicve",()=>{
     httpMock = null;
 
 
-  })
+  });
+
+  it("should use get with header",()=>{
+    //arrange
+     let service: ApiService;
+     let injector: TestBed;
+     let httpMock: HttpTestingController;
+
+     TestBed.configureTestingModule({
+       providers: [ApiService],
+       imports:[HttpClientTestingModule]
+     })
+
+     service=TestBed.get(ApiService);
+     injector=getTestBed();
+     httpMock = injector.get(HttpTestingController);
+
+     expect(service).toBeTruthy();
+
+     //act
+     const response ="testing";
+     const endpointParameter = "/testing"
+     const headerKey = "apellido";
+     const headerValue="portocarrero"
+
+     const header = new HttpHeaders().set(headerKey,headerValue);
+
+     //assert
+     service.get(endpointParameter,header).subscribe((response)=>{
+      expect(response).toBeTruthy();
+     })
+     //mocking the api request to generate a fake response
+        const req = httpMock.expectOne(environment.apiEndpoint+endpointParameter);
+        expect(req.request.method).toBe("GET");
+        expect(req.request.headers.get(headerKey)).toBe(headerValue);
+        req.flush(response);
+
+    //destroy
+    service=null;
+    injector= null;
+    httpMock=null;
+  });
+
+  it("should call post",()=>{
+    //arrange
+    let service :ApiService;
+    let injector : TestBed;
+    let httpMock : HttpTestingController;
+
+    TestBed.configureTestingModule({
+      providers: [ApiService],
+      imports: [HttpClientTestingModule]
+    })
+
+    service = TestBed.get(ApiService);
+
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+
+    expect(service).toBeTruthy();
+
+    //assert
+    const endpointParameter="/testing";
+    const bodyParameter={
+      name:"humberto"
+    };
+    const reqOpts={
+      key: "value"
+    }
+    service.post(endpointParameter,bodyParameter,reqOpts).subscribe((response)=>{
+       expect(response).toBeTruthy();
+    });
+    const req = httpMock.expectOne(environment.apiEndpoint+endpointParameter);
+    expect(req.request.method).toBe("POST");
+    req.flush(bodyParameter);
+
+    //destroy
+    service = null;
+    injector = null;
+    httpMock = null;
+  });
+
+  it("should call put",()=>{
+    //arrange
+    let service :ApiService;
+    let injector : TestBed;
+    let httpMock : HttpTestingController;
+
+    TestBed.configureTestingModule({
+      providers: [ApiService],
+      imports: [HttpClientTestingModule]
+    })
+
+    service = TestBed.get(ApiService);
+
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+
+    expect(service).toBeTruthy();
+
+    //assert
+    const endpointParameter="/testing";
+    const bodyParameter={
+      name:"humberto"
+    };
+    const reqOpts={
+      key: "value"
+    }
+    service.put(endpointParameter,bodyParameter,reqOpts).subscribe((response)=>{
+       expect(response).toBeTruthy();
+    });
+    const req = httpMock.expectOne(environment.apiEndpoint+endpointParameter);
+    expect(req.request.method).toBe("PUT");
+    req.flush(bodyParameter);
+
+    //destroy
+    service = null;
+    injector = null;
+    httpMock = null;
+  });
+
+  it("should call delete",()=>{
+    //arrange
+    let service :ApiService;
+    let injector : TestBed;
+    let httpMock : HttpTestingController;
+
+    TestBed.configureTestingModule({
+      providers: [ApiService],
+      imports: [HttpClientTestingModule]
+    })
+
+    service = TestBed.get(ApiService);
+
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+
+    expect(service).toBeTruthy();
+
+    //assert
+    const endpointParameter="/testing";
+    const bodyParameter={
+      name:"humberto"
+    };
+    service.delete(endpointParameter,bodyParameter).subscribe((response)=>{
+       expect(response).toBeTruthy();
+    });
+    const req = httpMock.expectOne(environment.apiEndpoint+endpointParameter);
+    expect(req.request.method).toBe("DELETE");
+    req.flush(bodyParameter);
+
+    //destroy
+    service = null;
+    injector = null;
+    httpMock = null;
+  });
 })
