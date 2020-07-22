a7016fd5505806972fa93186ead889871d1af8b6
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-07-21 23:21:43 -0500

    how to test http Services using injector and httpMock

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,73 @@
-import { TestBed } from '@angular/core/testing';
+import { ApiService } from "./api.service"
+import { TestBed, getTestBed } from "@angular/core/testing";
+import { HttpTestingController, HttpClientTestingModule } from "@angular/common/http/testing"
+import { environment } from "src/environments/environment";
 
-import { ApiService } from './api.service';
+fdescribe("api servicve",()=>{
+  it("should be constructed",()=>{
+    //arrange
+    let service: ApiService;
+    let injector: TestBed;
+    let httpMock: HttpTestingController;
 
-describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+    TestBed.configureTestingModule({
+       providers: [ApiService],
+       imports:[HttpClientTestingModule]
+    });
 
-  it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+    injector = getTestBed();
+    service = TestBed.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
+
+    //assert
+
     expect(service).toBeTruthy();
-  });
-});
+
+    //destroy
+    injector = null;
+    service = null;
+    httpMock = null;
+
+
+  })
+
+  it("should test the get",()=>{
+    //arrange
+    let service: ApiService;
+    let injector: TestBed;
+    let httpMock: HttpTestingController;
+
+    TestBed.configureTestingModule({
+       providers: [ApiService],
+       imports:[HttpClientTestingModule]
+    });
+
+    injector = getTestBed();
+    service = TestBed.get(ApiService);
+    httpMock = injector.get(HttpTestingController);
+
+
+    debugger;
+    const parameter = "/test";
+    const response = "testing";
+
+    //listening the httpGet
+    service.get(parameter).subscribe((response)=>{
+      //assert -> final comprobation.
+      expect(response).toBeTruthy();
+    });
+
+    //preparing the mocking
+      const req = httpMock.expectOne(environment.apiEndpoint + parameter);
+      expect(req.request.method).toBe('GET');
+
+      req.flush(response);
+
+    //destroy
+    injector = null;
+    service = null;
+    httpMock = null;
+
+
+  })
+})
